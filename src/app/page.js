e634188428ff3fe0529d5eaefde96a47262f0a22
Oliver <oliverahmed000@gmail.com>
2023-02-07 13:35:54 +0600

    installed Tailwind css

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <h1 className=" text-3xl font-bold  text-blue-600 "> hello </h1>
+    </div>
+  );
 };
 
 export default Page;
