704ce8000dd0efc0bcf167ef675bc54e2b7b93bd
Oliver <oliverahmed000@gmail.com>
2023-02-11 13:51:30 +0600

    data fetch from tmdb api

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,27 @@
+import Results from "@/components/Results";
 import React from "react";
+const API_KEY = process.env.API_KEY;
 
-const Page = () => {
+const Page = async ({ searchParams }) => {
+  const genre = searchParams.genre || " fetchTrending ";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await res.json();
+
+  const results = data.results;
+
   return (
     <div className="px-14 ">
-      <h1 className=" text-3xl font-bold  text-blue-600 "> hello </h1>
+      <Results results={results} />
     </div>
   );
 };
