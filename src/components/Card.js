4a48d6aeadbfbbfaddd8bfae85fb20993bd4734a
Oliver <oliverahmed000@gmail.com>
2023-02-14 10:31:13 +0600

    image source ('/') corrected

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ const Card = ({ result }) => {
     <div className=" mx-auto cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border-slate-400 sm:m-2 transition-shadow duration-200 group ">
       <Link href={`/movie/${result.id}`}>
         <Image
-          src={`https:/image.tmdb.org/t/p/original/${
+          src={`https://image.tmdb.org/t/p/original/${
             result.backdrop_path || result.poster_path
           }`}
           width={500}
